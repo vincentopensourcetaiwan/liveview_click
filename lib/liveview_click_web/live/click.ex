ebb9747644241ca57588b1517bfa90c85aaa35b1
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-11 15:30:46 +0800

    setup click binding

--- lib/liveview_click_web/live/click.ex
+++ lib/liveview_click_web/live/click.ex
@@ -3,12 +3,17 @@ defmodule LiveviewClickWeb.Click do
 
   def render(assigns) do
     ~H"""
-    <.button><%= @counter %></.button>
+    <.button phx-click="click"><%= @counter %></.button>
     """
   end
 
   def mount(_params, _session, socket) do
     socket = assign(socket, counter: 0)
     {:ok, socket}
+  end
+
+  def handle_event("click", _value, socket) do
+    socket = assign(socket, counter: socket.assigns.counter + 1)
+    {:noreply, socket}
   end
 end
